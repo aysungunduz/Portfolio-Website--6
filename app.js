c71ca3f532d1ae02bfb0702198250cff742a1ce7
aysun gunduz <113923715+aysungunduz@users.noreply.github.com>
2023-06-26 10:42:56 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const tablinks = document.querySelectorAll(".tab-links");
+const tabcontents = document.querySelectorAll(".tab-contents");
+
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link")
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab")
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+let sidemenu = document.querySelector("#sidemenu");
+
+function openmenu() {
+    sidemenu.style.right = "0"
+}
+
+function closemenu() {
+    sidemenu.style.right = "-200px"
+}
